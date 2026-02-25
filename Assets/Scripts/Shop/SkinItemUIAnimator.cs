5fe0a5633cc412f30c3bbd563679b3c19c9deb0e
Oleg Borisov <www.olegborisov1@gmail.com>
2026-02-25 14:17:35 +0300

    13.7 Added skin click animation

--- Assets/Scripts/Shop/SkinItemUIAnimator.cs
+++ Assets/Scripts/Shop/SkinItemUIAnimator.cs
@@ -2,20 +2,35 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class SkinItemUIAnimator : MonoBehaviour
+public class SkinItemUIAnimator : MonoBehaviour, IPointerClickHandler
 {
-    //[SerializeField] private RectTransform _rectTransform;
+    [SerializeField] private RectTransform _rectTransform;
 
-    //private Vector3 _animTargetScale;
-    //private float _animDuration = 1f;
+    [SerializeField] private Vector3 _animTargetScale;
+    [SerializeField] private float _animDuration = 0.2f;
+    [SerializeField] private Ease _easeType = Ease.OutBack;
+
+    private Vector3 _originScale;
 
     private void Awake()
     {
-        //_animTargetScale = _rectTransform.localScale * 0.95f;
+        _originScale = _rectTransform.localScale;
+        _animTargetScale = _originScale * 0.95f;
     }
 
-    //public void OnPointerClick(PointerEventData eventData)
-    //{
-    //    //_rectTransform.DOScale(_animTargetScale, _animDuration).SetEase(Ease.OutBack);
-    //}
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _rectTransform.DOScale(_animTargetScale, _animDuration / 2)
+            .SetEase(Ease.OutQuad)
+            .OnComplete(() =>
+            {
+                _rectTransform.DOScale(_originScale * 1.05f, _animDuration / 3)
+                    .SetEase(Ease.OutBounce)
+                    .OnComplete(() =>
+                    {
+                        _rectTransform.DOScale(_originScale, _animDuration / 4)
+                            .SetEase(Ease.OutBack);
+                    });
+            });
+    }
 }
